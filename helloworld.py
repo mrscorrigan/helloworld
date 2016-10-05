cced5e8c534012a404aa0a48e0f89d8e95cbd7a3
Stephen.Corrigan <stephen.corrigan@live.co.uk>
2016-10-05 17:29:48 -0400

    added some more extra functionality

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,24 @@
 
 print "hello, World!"
 
+
+
 prompt = 'What is your name'
 name = raw_input(prompt)
+age = "what is your age"
+age = raw_input(age)
 print "hello: %s" %name
+print "you are: %s" %age
+play = "Would you like to play a game (y/n)"
+play = raw_input(play)
+if play == "y":
+    print "you die"
+else:
+    print "good choice"
+
+#print "hello: %s" %name
+#print "you are: %s" %age
+#print "hello {0}, you are {1} years old! Would you like to play a game (y/n)".format(name,age, play)
 
 #input = raw_input('hello what is your name:')
 #print
